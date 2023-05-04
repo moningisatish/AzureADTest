e297175cc2735230bddeb497606a8a59834cf29f
moningisatish <moningisatish@gmail.com>
2023-05-04 20:26:13 +0530

    fixed the code for accessing the jwt token

--- src/main/java/org/example/controller/HomeController.java
+++ src/main/java/org/example/controller/HomeController.java
@@ -3,6 +3,9 @@ package org.example.controller;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -29,4 +32,20 @@ public class HomeController {
         String roles = auth.getAuthorities().toString();
         return "user: " + username + System.lineSeparator() + ", roles: " + roles;
     }
+
+    private final OAuth2AuthorizedClientService authorizedClientService;
+
+    public HomeController(OAuth2AuthorizedClientService authorizedClientService) {
+        this.authorizedClientService = authorizedClientService;
+    }
+
+    @GetMapping("/token")
+    public String token(OAuth2AuthenticationToken authentication) {
+        OAuth2AuthorizedClient authorizedClient =
+                this.authorizedClientService.loadAuthorizedClient(
+                        authentication.getAuthorizedClientRegistrationId(),
+                        authentication.getName());
+        return authorizedClient.getAccessToken().getTokenValue();
+    }
+
 }
